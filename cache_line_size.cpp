fbff91e744fb8a3925f2fdb06109cbf479a26217
Doctor <tixigam@gmail.com>
2025-10-11 16:11:08 +0300

    cache-benchmark

--- cache_line_size.cpp
+++ cache_line_size.cpp
@@ -79,17 +79,18 @@ void cache_assoc(){
         uint64_t t1 = rdtscp();
 
         double cyc = double(t1 - t0) / double(ITER);
+       // printf("%4zu,%.2f,%6zu\n", assoc_guess, cyc, stride);
         if(assoc_guess > 1 &&  1.4 < cyc/last){
             maxi = cyc/last;
             index = assoc_guess;
+            std::cout << "L1D assoc = " << index-1 << std::endl;
+            break;
         }
-       // printf("%4zu,%.2f,%6zu\n", assoc_guess, cyc, stride);
 
         last = cyc;
 
         FREE(mem);
     }
-    std::cout << "L1D assoc = " << index << std::endl;
 }
 
 void cache_size(){
